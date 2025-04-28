042ef45ea9545c196176f08387b6a74775dbf1fb
xkundrs00 <xkundrs00@stud.fit.vut.cz>
2025-04-28 19:14:37 +0200

    Initial Commit

---
+++ Tripity/TripityApp.swift
@@ -0,0 +1,32 @@
+//
+//  TripityApp.swift
+//  Tripity
+//
+//  Created by Samuel Kundrát on 28/04/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TripityApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ TripityTests/TripityTests.swift
@@ -0,0 +1,17 @@
+//
+//  TripityTests.swift
+//  TripityTests
+//
+//  Created by Samuel Kundrát on 28/04/2025.
+//
+
+import Testing
+@testable import Tripity
+
+struct TripityTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
